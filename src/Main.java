eca17a39793cfe385c4421192950911a9384ae4c
Your Name <your.email@example.com>
2025-06-04 17:32:39 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasingPrice = 100.00;
+        final double TAX_RATE = 0.05;
+        double salesTax = TAX_RATE * purchasingPrice;
+        double totalPrice = 0;
+        totalPrice = purchasingPrice + salesTax;
+        System.out.println("The cost is $" + purchasingPrice);
+        System.out.println("The sales tax is $" + salesTax);
+        System.out.println("The total price is $" + totalPrice);
+        System.out.println("Stupid");
+
+    }
+}
